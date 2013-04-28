3d281ee1dba8fb99f1a44e82cfd54974c215a0f2
changych <chenchangyuan0906@163.com>
2013-04-28 10:44:50 +0800

    follow by reddwarf

---
+++ GrandCloud_Storage/src/main/java/org/jclouds/GrandCloud/Storage/v1/StorageApiMetadata.java
@@ -0,0 +1,98 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.GrandCloud.Storage.v1;
+
+import static org.jclouds.openstack.keystone.v2_0.config.KeystoneProperties.CREDENTIAL_TYPE;
+import static org.jclouds.openstack.keystone.v2_0.config.KeystoneProperties.SERVICE_TYPE;
+
+import java.net.URI;
+import java.util.Properties;
+
+import org.jclouds.apis.ApiMetadata;
+import org.jclouds.openstack.keystone.v2_0.config.AuthenticationApiModule;
+import org.jclouds.openstack.keystone.v2_0.config.CredentialTypes;
+import org.jclouds.openstack.keystone.v2_0.config.KeystoneAuthenticationModule;
+import org.jclouds.openstack.keystone.v2_0.config.KeystoneAuthenticationModule.ZoneModule;
+import org.jclouds.GrandCloud.Storage.v1.config.StorageHttpApiModule;
+import org.jclouds.GrandCloud.Storage.v1.config.StorageParserModule;
+import org.jclouds.openstack.v2_0.ServiceType;
+import org.jclouds.rest.internal.BaseHttpApiMetadata;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Module;
+
+/**
+ * Implementation of {@link ApiMetadata} for RedDwarf API
+ * 
+ * @author Zack Shoylev
+ */
+public class StorageApiMetadata extends BaseHttpApiMetadata<StorageApi> {
+      
+   @Override
+   public Builder toBuilder() {
+      return new Builder().fromApiMetadata(this);
+   }
+
+   public StorageApiMetadata() {
+      this(new Builder());
+   }
+
+   protected StorageApiMetadata(Builder builder) {
+      super(builder);
+   }
+
+   public static Properties defaultProperties() {
+      Properties properties = BaseHttpApiMetadata.defaultProperties();
+      properties.setProperty(SERVICE_TYPE, ServiceType.DATABASE_SERVICE);      
+      properties.setProperty(CREDENTIAL_TYPE, CredentialTypes.PASSWORD_CREDENTIALS);
+      return properties;
+   }
+
+   public static class Builder extends BaseHttpApiMetadata.Builder<StorageApi, Builder> {
+
+      protected Builder() {         
+          id("openstack-reddwarf")
+         .name("OpenStack RedDwarf API")
+         .identityName("${tenantName}:${userName} or ${userName}, if your keystone supports a default tenant")
+         .credentialName("${password}")
+         .endpointName("Keystone base URL ending in /v2.0/")
+         .documentation(URI.create("http://api.openstack.org/"))
+         .version("1.0")
+         .defaultEndpoint("http://localhost:5000/v2.0/")
+         .defaultProperties(StorageApiMetadata.defaultProperties())
+         .defaultModules(ImmutableSet.<Class<? extends Module>>builder()
+                                     .add(AuthenticationApiModule.class)
+                                     .add(KeystoneAuthenticationModule.class)
+                                     .add(ZoneModule.class)
+                                     .add(StorageParserModule.class)
+                                     .add(StorageHttpApiModule.class)
+                                     .build());
+      }
+      
+      @Override
+      public StorageApiMetadata build() {
+         return new StorageApiMetadata(this);
+      }
+
+      @Override
+      protected Builder self() {
+         return this;
+      }
+   }
+}
